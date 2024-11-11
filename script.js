a2986817aa823c84c5e0b158dda341d33f10fa95
MertEfe96 <mertefe96@outlook.com>
2024-11-11 01:24:34 +0100

    added saveContacts function to PUT contacts into API

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-async function test() {
-  let URL =
-    "https://join-26d58-default-rtdb.europe-west1.firebasedatabase.app/";
-  let response = await fetch(URL + ".json");
-  let responseToJson = await response.json();
-  console.log(responseToJson);
-}
+// const BASE_URL =
+//   "https://join-26d58-default-rtdb.europe-west1.firebasedatabase.app/";
+
+// async function test() {
+//   let URL =
+//     "https://join-26d58-default-rtdb.europe-west1.firebasedatabase.app/";
+//   let response = await fetch(URL + ".json");
+//   let responseToJson = await response.json();
+//   console.log(responseToJson);
+// }
 
 /**
  * Function opens contact-details after a contact has been clicked at the cantact-list
  */
 function openContact() {
-  const contactMainContainer = document.getElementById('contactMainContainer');
-  const toggleCheckBox = document.getElementById('toggleContactCard');
+  const contactMainContainer = document.getElementById("contactMainContainer");
+  const toggleCheckBox = document.getElementById("toggleContactCard");
 
   if (toggleCheckBox.checked) {
-    contactMainContainer.style.display = 'none';
-    contactMainContainer.classList.remove('show');
+    contactMainContainer.style.display = "none";
+    contactMainContainer.classList.remove("show");
   } else {
-    contactMainContainer.style.display = 'flex';
-    setTimeout(() => contactMainContainer.classList.add('show'), 10);
+    contactMainContainer.style.display = "flex";
+    setTimeout(() => contactMainContainer.classList.add("show"), 10);
   }
 }
 
@@ -26,27 +29,27 @@ function openContact() {
  * Function opens overlay-window to edit a contact
  */
 function editContact() {
-  const overlayMain = document.getElementById('contactOverlayEditMain');
-  const overlayEdit = document.getElementById('contactOverlayEdit');
+  const overlayMain = document.getElementById("contactOverlayEditMain");
+  const overlayEdit = document.getElementById("contactOverlayEdit");
 
-  overlayMain.style.display = 'flex';
-  setTimeout(() => overlayMain.classList.add('show'), 10);
-  setTimeout(() => overlayEdit.classList.add('show'), 10);
+  overlayMain.style.display = "flex";
+  setTimeout(() => overlayMain.classList.add("show"), 10);
+  setTimeout(() => overlayEdit.classList.add("show"), 10);
 }
 
 /**
  * Function closes overlay-window
  */
 function closeContactEdit() {
-  const overlayEdit = document.getElementById('contactOverlayEdit');
-  const overlayMain = document.getElementById('contactOverlayEditMain');
+  const overlayEdit = document.getElementById("contactOverlayEdit");
+  const overlayMain = document.getElementById("contactOverlayEditMain");
 
-  overlayEdit.classList.remove('show');
+  overlayEdit.classList.remove("show");
   setTimeout(() => {
-    overlayMain.classList.remove('show');
+    overlayMain.classList.remove("show");
 
     setTimeout(() => {
-      overlayMain.style.display = 'none';
+      overlayMain.style.display = "none";
     }, 100);
   }, 100);
 }
@@ -55,26 +58,26 @@ function closeContactEdit() {
  * function opens add-contact-form
  */
 function addContact() {
-  const addContactMain = document.getElementById('addContactMain');
-  const addContact = document.getElementById('addContact');
+  const addContactMain = document.getElementById("addContactMain");
+  const addContact = document.getElementById("addContact");
 
-  addContactMain.style.display = 'flex';
-  setTimeout(() => addContactMain.classList.add('show'), 10);
-  setTimeout(() => addContact.classList.add('show'), 10);
+  addContactMain.style.display = "flex";
+  setTimeout(() => addContactMain.classList.add("show"), 10);
+  setTimeout(() => addContact.classList.add("show"), 10);
 }
 
 /**
  * function closes add-contact-form
  */
 function closeAddContact() {
-  const addContactMain = document.getElementById('addContactMain');
-  const addContact = document.getElementById('addContact');
+  const addContactMain = document.getElementById("addContactMain");
+  const addContact = document.getElementById("addContact");
 
-  addContact.classList.remove('show');
+  addContact.classList.remove("show");
   setTimeout(() => {
-    addContactMain.classList.remove('show');
+    addContactMain.classList.remove("show");
     setTimeout(() => {
-      addContactMain.style.display = 'none';
+      addContactMain.style.display = "none";
     }, 100);
   }, 100);
   emptyInputAddContact();
@@ -84,15 +87,32 @@ function closeAddContact() {
  * function empties input-value from add-contact-form
  */
 function emptyInputAddContact() {
-  document.getElementById('addContactInputName').value = '';
-  document.getElementById('addContactInputMail').value = '';
-  document.getElementById('addContactInputPhone').value = '';
+  document.getElementById("addContactInputName").value = "";
+  document.getElementById("addContactInputMail").value = "";
+  document.getElementById("addContactInputPhone").value = "";
 }
 
 /**
  * function closes/deletes contact-details and contact-card
  */
 function closeContact() {
-  document.getElementById('contactMainContainer').style.display = 'none';
-  document.getElementById('labelContactCard').style.display = 'none';
+  document.getElementById("contactMainContainer").style.display = "none";
+  document.getElementById("labelContactCard").style.display = "none";
+}
+
+async function saveContact(path = "", data = "") {
+  const BASE_URL =
+    "https://join-26d58-default-rtdb.europe-west1.firebasedatabase.app/";
+  let name = document.getElementById("addContactInputName").value;
+  let mail = document.getElementById("addContactInputMail").value;
+  let phone = document.getElementById("addContactInputPhone").value;
+  data = {name: name, email: mail, number: phone};
+  path = name.toLowerCase();
+  let response = await fetch(BASE_URL + "contacts/.json", {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 }
