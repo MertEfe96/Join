6e8fb2a1df94ce65e701e145a9cac242e176f908
MertEfe96 <mertefe96@outlook.com>
2024-11-15 20:59:12 +0100

    added the dropdown menu

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function closeLogin() {
   let div = document.getElementById("loginContent");
   div.innerHTML = "";
 }
+
+function showDropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".userIcon")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
