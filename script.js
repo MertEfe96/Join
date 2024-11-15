316c291b13244faa83b9d568f504db29b0db18c2
MertEfe96 <mertefe96@outlook.com>
2024-11-15 20:20:04 +0100

    integrated the Login template to all sites

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const BASE_URL =
   "https://join-26d58-default-rtdb.europe-west1.firebasedatabase.app/";
+
+function renderLogin() {
+  let div = document.getElementById("loginContent");
+  div.innerHTML = loginTemplate();
+}
+
+function closeLogin() {
+  let div = document.getElementById("loginContent");
+  div.innerHTML = "";
+}
