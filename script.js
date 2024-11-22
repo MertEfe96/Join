4b4718b8b69966aa13e33c84755b552df62d2326
MertEfe96 <mertefe96@outlook.com>
2024-11-22 19:26:25 +0100

    merge

--- script.js
+++ script.js
@@ -11,11 +11,10 @@ function closeLogin() {
   div.innerHTML = "";
 }
 
-function renderLogin(){
+function renderLogin() {
   let div = document.getElementById("loginContent");
   div.innerHTML = loginTemplate();
 }
-
 
 document.addEventListener("click", function (e) {
   let dropdown = document.getElementById("dropdownContent");
@@ -86,53 +85,61 @@ function setMinDate() {
   document.getElementsByName("date")[0].min = today;
 }
 
-async function postSignUp(data = ""){
+async function postSignUp(data = "") {
+  let nameSignup = document.getElementById("nameSignUp").value;
+  let emailSignUp = document.getElementById("emailSignUp").value;
+  let passwordSignUp = document.getElementById("passwordSignUp").value;
+  let passwordConfirmSignUp = document.getElementById(
+    "passwordConfirmSignUp"
+  ).value;
+  let userColor = AddColorToUser();
+  data = {
+    name: nameSignup,
+    email: emailSignUp,
+    password: passwordSignUp,
+    passwordconfirm: passwordConfirmSignUp,
+    color: userColor,
+  };
+  await fetch(BASE_URL + "users/.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  clearInputSignUp();
+  renderLogin();
+}
 
-    let nameSignup = document.getElementById("nameSignUp").value;
-    let emailSignUp = document.getElementById("emailSignUp").value;
-    let passwordSignUp = document.getElementById("passwordSignUp").value;
-    let passwordConfirmSignUp = document.getElementById("passwordConfirmSignUp").value;
-    let userColor = AddColorToUser();
-    data = {name: nameSignup, email: emailSignUp, password: passwordSignUp, passwordconfirm: passwordConfirmSignUp, color: userColor};
-    await fetch(BASE_URL + "users/.json", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    clearInputSignUp();
-    renderLogin();
-  }
+async function loginRequest() {
+  let response = await fetch(BASE_URL + ".json");
+  let data = await response.json();
+  let users = data.users;
+  let emailLogin = document.getElementById("emailLogin").value;
+  let passwordLogin = document.getElementById("passwordLogin").value;
 
-
- async function loginRequest(){
-
-      let response = await fetch(BASE_URL + ".json");
-      let data = await response.json();
-      let users = data.users;
-      let emailLogin = document.getElementById("emailLogin").value;
-      let passwordLogin = document.getElementById("passwordLogin").value;
-    
-      for (let userId in users) {
-        if (users[userId].email === emailLogin && users[userId].password === passwordLogin) {
-            console.log("Erfolgreich Angemeldet");
-            clearInputLogin();
-            break;
-        }else{
-          console.log("Email oder Password ist Falsch !");
-        }
+  for (let userId in users) {
+    if (
+      users[userId].email === emailLogin &&
+      users[userId].password === passwordLogin
+    ) {
+      console.log("Erfolgreich Angemeldet");
+      clearInputLogin();
+      break;
+    } else {
+      console.log("Email oder Password ist Falsch !");
     }
   }
+}
 
-  function clearInputSignUp(){
-      document.getElementById("nameSignUp").value = "";
-      document.getElementById("emailSignUp").value = "";
-      document.getElementById("passwordSignUp").value = "";
-      document.getElementById("passwordConfirmSignUp").value = "";
-    }
+function clearInputSignUp() {
+  document.getElementById("nameSignUp").value = "";
+  document.getElementById("emailSignUp").value = "";
+  document.getElementById("passwordSignUp").value = "";
+  document.getElementById("passwordConfirmSignUp").value = "";
+}
 
-    function clearInputLogin(){
-      document.getElementById("emailLogin").value = "";
-      document.getElementById("passwordLogin").value = "";
-    }
+function clearInputLogin() {
+  document.getElementById("emailLogin").value = "";
+  document.getElementById("passwordLogin").value = "";
+}
